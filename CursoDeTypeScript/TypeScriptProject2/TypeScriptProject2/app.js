eda476ce63bc920f3301b4b4f8e017fbe228a0a7
Leonardo Godinho <godinho.leo@gmail.com>
2018-04-27 19:18:52 -0300

    Project2

---
+++ CursoDeTypeScript/TypeScriptProject2/TypeScriptProject2/app.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    // Number
+    var pi; // Ponto flutuante
+    pi = 3.1415;
+    var distance = 2345;
+    alert("The pi number is " + pi + "\nDistance: " + distance);
+    // Boolean
+    var isGood;
+    isGood = true;
+    alert("Is good? " + isGood);
+    // String
+    var myCar;
+    myCar = "Ferrari F-50";
+    var myCity = 'São Paulo';
+    alert("My car: " + myCar + "\nMy City: " + myCity);
+};
+//# sourceMappingURL=app.js.map
---
+++ CursoDeTypeScript/TypeScriptProject2/TypeScriptProject2/app.ts
@@ -0,0 +1,27 @@
+﻿window.onload = () => {
+
+    // Number
+    var pi: number; // Ponto flutuante
+    pi = 3.1415;
+
+    var distance: number = 2345;
+
+    alert("The pi number is " + pi + "\nDistance: " + distance);
+
+
+    // Boolean
+    var isGood: boolean;
+    isGood = true;
+
+    alert("Is good? " + isGood);
+
+
+    // String
+    var myCar: string;
+    myCar = "Ferrari F-50";
+
+    var myCity: string = 'São Paulo';
+
+    alert("My car: " + myCar + "\nMy City: " + myCity);
+
+}
