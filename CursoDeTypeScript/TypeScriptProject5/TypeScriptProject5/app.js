4804fbafb22aca88e3e00804695d0a7f2b6dad4c
Leonardo Godinho <godinho.leo@gmail.com>
2018-04-30 14:08:07 -0300

    Project5

---
+++ CursoDeTypeScript/TypeScriptProject5/TypeScriptProject5/app.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    var number1 = 73;
+    var number2 = 3;
+    //alert('number1 + number2 = ' + (number1 + number2));
+    var text1 = "Leonardo ";
+    var text2 = "Godinho";
+    //alert('text1 + text 2 = ' + (text1 + text2));
+    //alert('number1 - number2 = ' + (number1 - number2));
+    //alert('number1 * number2 = ' + (number1 * number2));
+    //alert('number1 / number2 = ' + (number1 / number2));
+    //alert('number1 % number2 = ' + (number1 % number2));
+    //alert('number1 & number2 = ' + (number1 & number2));
+    //alert('number1 | number2 = ' + (number1 | number2));
+    //alert('number1 ^ number2 = ' + (number1 ^ number2)); // ou exclusivo
+    //alert('number1 += number2 = ' + (number1 += number2));
+    //alert('number1 =- number2 = ' + (number1 =- number2));
+    number1++;
+    //alert('number1++ = ' + number1);
+    --number2;
+    //alert('--number2 = ' + number2);
+    var isTrue = true;
+    alert('!isTrue = ' + !isTrue);
+};
+//# sourceMappingURL=app.js.map
---
+++ CursoDeTypeScript/TypeScriptProject5/TypeScriptProject5/app.ts
@@ -0,0 +1,37 @@
+﻿window.onload = () => {
+
+    var number1: number = 73;
+    var number2: number = 3;
+    //alert('number1 + number2 = ' + (number1 + number2));
+
+    var text1: string = "Leonardo ";
+    var text2: string = "Godinho";
+    //alert('text1 + text 2 = ' + (text1 + text2));
+
+    //alert('number1 - number2 = ' + (number1 - number2));
+
+    //alert('number1 * number2 = ' + (number1 * number2));
+
+    //alert('number1 / number2 = ' + (number1 / number2));
+
+    //alert('number1 % number2 = ' + (number1 % number2));
+
+    //alert('number1 & number2 = ' + (number1 & number2));
+
+    //alert('number1 | number2 = ' + (number1 | number2));
+
+    //alert('number1 ^ number2 = ' + (number1 ^ number2)); // ou exclusivo
+
+    //alert('number1 += number2 = ' + (number1 += number2));
+
+    //alert('number1 =- number2 = ' + (number1 =- number2));
+
+    number1++;
+    //alert('number1++ = ' + number1);
+
+    --number2;
+    //alert('--number2 = ' + number2);
+
+    var isTrue: boolean = true;
+    alert('!isTrue = ' + !isTrue);
+};
