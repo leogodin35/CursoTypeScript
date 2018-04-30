b826d5b29f554bd74819b3311ce4c4de893a1cde
Leonardo Godinho <godinho.leo@gmail.com>
2018-04-30 16:30:33 -0300

    Project11

---
+++ CursoDeTypeScript/TypeScriptProject11/TypeScriptProject11/app.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    //try {
+    //    alertABC("Hello World");
+    //}
+    //catch (err) {
+    //    document.getElementById('content').innerHTML = err.message;
+    //}
+    function myCheck(myVar) {
+        if (myVar == "")
+            throw new Error("Empty"); // Cria um novo tipo de erro
+        if (myVar > 100)
+            throw new Error("Too High");
+        if (myVar < 10)
+            throw new Error("Too Low");
+    }
+    try {
+        myCheck(50);
+        alert("OK");
+    }
+    catch (err) {
+        document.getElementById('content').innerHTML = "<span style='color:#ff0000'>" + err.message + "</span>";
+    }
+    finally {
+        var varContent = document.getElementById('content').innerHTML;
+        document.getElementById('content').innerHTML = varContent + "<br>Hello World FINALLY";
+    }
+};
+//# sourceMappingURL=app.js.map
---
+++ CursoDeTypeScript/TypeScriptProject11/TypeScriptProject11/app.ts
@@ -0,0 +1,28 @@
+﻿window.onload = () => {
+
+    //try {
+    //    alertABC("Hello World");
+    //}
+    //catch (err) {
+    //    document.getElementById('content').innerHTML = err.message;
+    //}
+
+
+    function myCheck(myVar: any) {
+        if (myVar == "") throw new Error("Empty"); // Cria um novo tipo de erro
+        if (myVar > 100) throw new Error("Too High");
+        if (myVar < 10) throw new Error("Too Low");        
+    }
+
+    try {
+        myCheck(50);
+        alert("OK");
+    }
+    catch (err) {
+        document.getElementById('content').innerHTML = "<span style='color:#ff0000'>" + err.message + "</span>";
+    }
+    finally {
+        var varContent: string = document.getElementById('content').innerHTML;
+        document.getElementById('content').innerHTML = varContent + "<br>Hello World FINALLY";
+    }
+};
